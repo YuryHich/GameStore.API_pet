293f32793e40f1e6808dda0df26e5484439bd791
Yurec <gicurij78@gmail.com>
2026-03-22 02:51:52 +0300

    ASP.NET 3h course

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,68 @@
+using GameStore.Api.Dtos;
+const string GetGameEndpointName = "GetGame";
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+List<GameDto> games = [
+    new (
+        1,
+         "Street Fighter 11",
+          "Figthing",
+           19.19M, 
+           new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Elden Ring",
+        "Souls-like",
+        20.00M,
+        new DateOnly(2023, 4, 27 )),
+    new (
+        3,
+        "God of War",
+        "Action RPG",
+        25.00M,
+        new DateOnly(2021, 5, 28))
+];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+
+
+// GET /games/1
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+.WithName(GetGameEndpointName);
+
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count+1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
+
+// PUT /games/1
+
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+    return Results.NoContent();
+});
+
+app.Run();
